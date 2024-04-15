c24073ec5f93c9862b9d3751fca5c4e137dbb57c
Araceli Noemí Fernández <aracelifernandez.3006@gmail.com>
2024-04-15 20:37:20 -0300

    Primer commit, creacion de Package calculos y modelos. Clase Audio, Cancion y Podcast

---
+++ src/io/araf/radiosound/modelos/Audio.java
@@ -0,0 +1,57 @@
+package io.araf.radiosound.modelos;
+
+public class Audio {
+    private String titulo;
+    private int duracion;
+    private int totalReproducciones;
+    private int totalDeMeGusta;
+    private int clasificacion;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public int getTotalReproducciones() {
+        return totalReproducciones;
+    }
+
+    public void setTotalReproducciones(int totalReproducciones) {
+        this.totalReproducciones = totalReproducciones;
+    }
+
+    public int getTotalDeMeGusta() {
+        return totalDeMeGusta;
+    }
+
+    public void setTotalDeMeGusta(int totalDeMeGusta) {
+        this.totalDeMeGusta = totalDeMeGusta;
+    }
+
+    public int getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(int clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    private void meGusta() {
+        this.totalDeMeGusta++;
+    }
+
+    void reproduce() {
+        this.totalReproducciones++;
+    }
+}
